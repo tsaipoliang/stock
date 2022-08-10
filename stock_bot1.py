9e942b0c580ed247aee881409ad146a0e2aa829e
tsaipoliang <tsaipoliang@gmail.com>
2022-08-10 21:53:18 +0800

    45

--- stock_bot1.py
+++ stock_bot1.py
@@ -46,7 +46,8 @@ def callback():
 
     # get request body as text
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+    get_stock=yahoo_stock_crawler(body)
+    app.logger.info("Request body: " + get_stock)
 
     # handle webhook body
     try:
