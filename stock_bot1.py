6a2f81436caef3629bc94386680ef8a3a358fbe1
tsaipoliang <tsaipoliang@gmail.com>
2022-08-10 21:01:32 +0800

    test

--- stock_bot1.py
+++ stock_bot1.py
@@ -69,7 +69,7 @@ if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
 
-get_stock=yahoo_stock_crawler('2330.TW')
+#get_stock=yahoo_stock_crawler('2330.TW')
 #line_bot_api.push_message('U9a880705aba3434ff1b4c8bcad222f79', TextSendMessage(text=get_stock))
 
 
